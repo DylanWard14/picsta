eda2ced84890acefcf204435d293b076b0e99e3e
DylanWard14 <dylan.ward@iinet.net.au>
2022-01-29 11:49:44 +1000

    Created users table migration and sample seed data.

---
+++ api-gateway/knexfile.ts
@@ -0,0 +1,56 @@
+import type { Knex } from "knex";
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "insta-dev",
+      user: "postgres",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./seeds/dev",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "insta-staging",
+      user: "postgres",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "insta-prod",
+      user: "postgres",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+module.exports = config;
---
+++ api-gateway/migrations/20220129005536_create_users_table.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id");
+    table.timestamp("created_at").notNullable().defaultTo(knex.fn.now());
+    table.timestamp("updated_at").notNullable().defaultTo(knex.fn.now());
+    table.string("username", 30).notNullable();
+    table.string("bio", 300).nullable();
+    table.string("avatar_url").nullable();
+    table.string("email", 50).notNullable();
+    table.string("password", 50).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("users");
+}
