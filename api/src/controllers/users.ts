8049c1fa1382b3523f7f36cfddf25924d55b5e99
DylanWard14 <dylan.ward@iinet.net.au>
2022-02-05 11:59:42 +1000

    Added route and controller for fetching a single user

--- api/src/controllers/users.ts
+++ api/src/controllers/users.ts
@@ -4,14 +4,14 @@ import { User } from "../models/user";
 import { database } from "../../knexfile";
 import { GenericQuery } from "../types";
 
-type UserRequest = Request<
+type UsersRequest = Request<
   {},
   {},
   {},
   GenericQuery<User & { password: string }>
 >;
 
-const defaultSelect = [
+const defaultSelect: Array<keyof User> = [
   "id",
   "created_at",
   "updated_at",
@@ -20,27 +20,72 @@ const defaultSelect = [
   "avatar_url",
 ];
 
-export const getUsers = async (req: UserRequest, res: Response) => {
+const isUserKey = (key: string): key is keyof User => {
+  return [
+    "id",
+    "created_at",
+    "updated_at",
+    "username",
+    "bio",
+    "avatar_url",
+  ].includes(key);
+};
+
+export const getUsers = async (req: UsersRequest, res: Response) => {
   try {
     const { limit, skip, select, ...where } = req.query;
     // Not sure if the best way to stop people from selecting/quering for the password
     delete where.password;
 
-    const computedSelect = select
-      ? select.split(",").filter((value) => value !== "password")
+    const computedSelect: Array<keyof User> = select
+      ? select.split(",").reduce<Array<keyof User>>((acc, item) => {
+          if (isUserKey(item)) {
+            return [...acc, item];
+          }
+          return acc;
+        }, [])
       : defaultSelect;
 
     const users = await database<User>("users")
       .where(where)
       .select(computedSelect)
       .offset(skip ?? 0)
-      .modify((builder) => {
+      .modify<{}, User[]>((builder) => {
         if (limit) {
           builder.limit(limit);
         }
+        return builder;
       });
 
     res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+type UserRequest = Request<{ id: string }, {}, {}, { select?: string }>;
+
+export const getUser = async (req: UserRequest, res: Response) => {
+  try {
+    const { select } = req.query;
+    const computedSelect: Array<keyof User> = select
+      ? select.split(",").reduce<Array<keyof User>>((acc, item) => {
+          if (isUserKey(item)) {
+            return [...acc, item];
+          }
+          return acc;
+        }, [])
+      : defaultSelect;
+
+    const user = await database<User>("users")
+      .where(req.params)
+      .first(computedSelect);
+
+    if (!user) {
+      res.status(404).json({ error: "user not found" });
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error });
   }
--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { getUsers } from "../controllers/users";
+import { getUsers, getUser } from "../controllers/users";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
+
+userRouter.get("/:id", getUser);
 
 export { userRouter };
